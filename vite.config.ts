9cfdcf390320abd99c65be32b7460825a4d3528e
sankar <sssankarsam@gmail.com>
2026-05-30 16:53:36 +0530

    vite config change

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
   // Since your site is deploying to the root domain https://sankarsans.github.io/
