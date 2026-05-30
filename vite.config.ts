ccf6ea5f1a37ac529ccf295948673dbc6dc64bb8
sankar <sssankarsam@gmail.com>
2026-05-30 16:36:45 +0530

    vite config change

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // Since your site is deploying to the root domain https://sankarsans.github.io/
+  base: "/",
+});
